127d88192c60133510ff30c7e5cc63a087ea4bd6
KholofeloLekala <2481434@students.wits.ac.za>
2023-03-29 21:46:44 +0200

    kholo's second build

--- signup2.js
+++ signup2.js
@@ -1,10 +1,18 @@
 document.querySelector("#validate").addEventListener("click", e => {
     e.preventDefault();
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
     var username = document.getElementById("Username").value;
     var password = document.getElementById("Password").value;
     var confirm = document.getElementById("Confirm").value;
     var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!regexp.test(String(username).toLowerCase())){
+    if (name==""){
+        alert("enter name");
+    }
+    else if (surname==""){
+        alert("enter surname");
+    }
+    else if (!regexp.test(String(username).toLowerCase())){
         alert("invalide email");
     }
     else if (password.length<6){
