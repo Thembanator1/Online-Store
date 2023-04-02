ce41ce1b855d357693dcf0192e10e06d8a3474cf
Aluzuko <2456615@students.wits.ac.za>
2023-04-02 19:19:16 +0200

    Added comments and checkIUserExist

--- login2.js
+++ login2.js
@@ -11,30 +11,45 @@ const firebaseConfig = {
 };
 // initialize firebase
 firebase.initializeApp(firebaseConfig);
-// reference your database
+// get reference to the Firebase database
 var contactFormDB = firebase.database().ref("onlinestore");
 
+// add an event listener to the "validate" button on the form
 document.querySelector("#validate").addEventListener("click", e => {
     e.preventDefault();
+    
+    // get the email and password values from the form
     var email = document.getElementById("Email").value;
     var password = document.getElementById("Password").value;
 
-    //var pass = "return the password where the email is equal to the username above"
+    // initialize the state variable to "User does not exist"
     var state = "User does not exist";
+
+    // attach a listener to the Firebase database reference
+    // this listener will be called whenever the value of the database changes
+    // it will iterate over all the child snapshots of the "onlinestore" node
     contactFormDB.on("value", function (snapshot) {        
         snapshot.forEach(function(childSnapshot) {
+            
+            // check if the email and password match a record in the database
+            // if so, set the state to "successful"
             if (email==childSnapshot.val().email && password==childSnapshot.val().password){
                 state = "successful";
             }    
+            
+            // if the email matches but the password does not, set the state to "Invalid password"
             else if (email==childSnapshot.val().email){
                 state = "Invalid password";
             }
         });
+
+        // after iterating over all the child snapshots, check the state and take appropriate action
         if (state=="successful"){
-            window.location.assign("homepage.html");
+            window.location.assign("homepage.html"); // redirect to the homepage
         }
         else {
-            alert(state);
+            alert(state); // show an alert with the state message
         }
     });
 });
+
