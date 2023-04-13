6585e1fc2c9b8ffc42db27cc49b0ef172de00d8b
Aluzuko <2456615@students.wits.ac.za>
2023-04-13 19:41:39 +0200

    updated buttons

--- checkout.js
+++ checkout.js
@@ -1,3 +1,10 @@
+var next=document.getElementById("next-button");
+var prev=document.getElementById("prev-button");
+var done=document.getElementById("done");
+if(form1.style.display !== "none"){
+    prev.style.display="none";
+    done.style.display="none";
+}
 function nextView() {
     var form1 = document.getElementById("form1");
     var form2 = document.getElementById("form2");
@@ -6,10 +13,13 @@ function nextView() {
         form1.style.display = "none";
         form2.style.display = "block";
         form3.style.display = "none";
+        prev.style.display="block";
     } else if (form2.style.display !== "none") {
         form1.style.display = "none";
         form2.style.display = "none";
         form3.style.display = "block";
+        next.style.display="none";
+        done.style.display="block";
     } else {
         form1.style.display = "none";
         form2.style.display = "none";
@@ -25,15 +35,21 @@ function prevView() {
         form1.style.display = "none";
         form2.style.display = "block";
         form3.style.display = "none";
+        next.style.display="block";
+        done.style.display="none";
     } else if (form2.style.display !== "none") {
         form1.style.display = "block";
         form2.style.display = "none";
         form3.style.display = "none";
+        prev.style.display="none";
     } else {
         form1.style.display = "block";
         form2.style.display = "none";
         form3.style.display = "none";
     }
+}
+function doneView(){
+    window.location.assign("homepage.html");
 }
 
 // var switchButton = document.getElementById("switch-button");
