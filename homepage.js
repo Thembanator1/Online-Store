daaf76ca3d0e1a47c1eb91a8957e84b1ac8b8253
Aluzuko <2456615@students.wits.ac.za>
2023-04-20 14:23:53 +0200

    made items clickable

--- homepage.js
+++ homepage.js
@@ -13,20 +13,30 @@ const firebaseConfig = {
   // initialize firebase
   // initialize Firebase using the provided configuration
   firebase.initializeApp(firebaseConfig);
-  // reference your database
-  var imagesRef = firebase.database().ref("Products");
+  // Reference your database
+// reference your database
+var imagesRef = firebase.database().ref("Products");
 
-  
-  const productsList = document.querySelector(".image-container");
+const productsList = document.querySelector(".image-container");
 
 // Clear any existing product items from the list
 productsList.innerHTML = "";
 
 // Retrieve all product items from the Firebase Realtime Database
 const productsRef = firebase.database().ref("Products");
-productsRef.on("value", snapshot => {
+productsRef.once("value", snapshot => {
   const products = snapshot.val();
-  for (const [key, product] of Object.entries(products)) {
+
+  // Shuffle the products randomly using Fisher-Yates algorithm
+  const shuffledProducts = Object.entries(products);
+  for (let i = shuffledProducts.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledProducts[i], shuffledProducts[j]] = [shuffledProducts[j], shuffledProducts[i]];
+  }
+
+  // Loop through only 60% of the shuffled products
+  const numProducts = Math.floor(shuffledProducts.length * 0.6);
+  for (const [key, product] of shuffledProducts.slice(0, numProducts)) {
     // Create a new list item for each product item
     const listItem = document.createElement("div");
     listItem.className = "product-item";
@@ -54,12 +64,82 @@ productsRef.on("value", snapshot => {
     priceSpan.textContent = "$" + product.price;
     infoDiv.appendChild(priceSpan);
 
+    // Add an event listener to the list item
+    listItem.addEventListener("click", () => {
+      localStorage.setItem('item_id', key);
+//const email = localStorage.getItem('item_id');
+      console.log(key); // Print the key of the clicked item
+    });
+
     // Add the list item to the products list
     productsList.appendChild(listItem);
   }
 });
 
-  
+
+
+const verticalContainer = document.querySelector('.vertical-container');
+
+// Retrieve all product items from the Firebase Realtime Database
+//const productsRef = firebase.database().ref('Products');
+productsRef.on('value', snapshot => {
+  // Clear any existing product items from the container
+  verticalContainer.innerHTML = '';
+
+  const products = snapshot.val();
+
+  // Create a container for each two products
+  let productContainer;
+  let count = 0;
+
+  for (const [key, product] of Object.entries(products)) {
+    // Create a new container for every two products
+    if (count % 2 === 0) {
+      productContainer = document.createElement('div');
+      productContainer.className = 'product-container';
+      verticalContainer.appendChild(productContainer);
+    }
+
+    // Create a new list item for the product
+    const listItem = document.createElement('div');
+    listItem.className = 'product-item';
+
+    // Create an image element to display the product image
+    const image = document.createElement('img');
+    image.src = product.picture;
+    image.alt = product.name;
+    listItem.appendChild(image);
+
+    // Create a div element to hold the product name and price
+    const infoDiv = document.createElement('div');
+    infoDiv.className = 'product-info';
+    listItem.appendChild(infoDiv);
+
+    // Create a span element to display the product name
+    const nameSpan = document.createElement('span');
+    nameSpan.className = 'product-name';
+    nameSpan.textContent = product.name;
+    infoDiv.appendChild(nameSpan);
+
+    // Create a span element to display the product price
+    const priceSpan = document.createElement('span');
+    priceSpan.className = 'product-price';
+    priceSpan.textContent = '$' + product.price;
+    infoDiv.appendChild(priceSpan);
+
+    listItem.addEventListener("click", () => {
+      localStorage.setItem('item_id', key);
+//const email = localStorage.getItem('item_id');
+
+      console.log(key); // Print the key of the clicked item
+    });
+    // Add the list item to the product container
+    productContainer.appendChild(listItem);
+
+    count++;
+  }
+});
+
 
 
 const slider = document.querySelector(".slider");
