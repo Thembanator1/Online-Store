6d7d113fa1f1e0d66710734ce7f2b7262c06b477
2446026@students.wits.ac.za <2446026@students.wits.ac.za>
2023-05-13 19:21:29 +0200

    review works but does not take product id from page

--- order_details.js
+++ order_details.js
@@ -1,3 +1,23 @@
+const firebaseConfig = {
+    //   copy your firebase config informations
+    apiKey: "AIzaSyBiQr7aHxdYxk8sCkHxMebkVyBEgXCnknU",
+    authDomain: "online-store-b90ca.firebaseapp.com",
+    databaseURL: "https://online-store-b90ca-default-rtdb.firebaseio.com",
+    projectId: "online-store-b90ca",
+    storageBucket: "online-store-b90ca.appspot.com",
+    messagingSenderId: "160581372978",
+    appId: "1:160581372978:web:b507d7ac5f14c9e4ff002b",
+    measurementId: "G-PH4QNCPP2J"
+};
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+// get reference to the Firebase database
+var contactFormDB = firebase.database().ref("Cart");
+var user = localStorage.getItem('user_email');
+var reviewsRef = firebase.database().ref('Reviews');
+var ids=localStorage.getItem("shipping");
+const supplir_emailRef = firebase.database().ref('Shipping/' + ids + '/supplier_email');
+
 var imageUrl  = localStorage.getItem("image");
 var nname = localStorage.getItem("name");
 var price = localStorage.getItem("price");
@@ -6,7 +26,6 @@ var paym = localStorage.getItem("paym");
 var deliv = localStorage.getItem("deliv");
 var desc = localStorage.getItem("description");
 var quantity = localStorage.getItem("quantity");
-
 
 const img = document.getElementById('product-img');
 img.src = imageUrl;
